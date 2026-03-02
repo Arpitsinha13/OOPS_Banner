ae4319628eb27ecf08c79b37bc510140e4e11173
Arpitsinha13 <arpitbinaysinha07@gmail.com>
2026-03-02 13:13:30 +0530

    This is UC1

--- OOPS.java
+++ OOPS.java
@@ -1,5 +1,13 @@
+//This is UC 1
+/*
+
+@author Arpit
+@version 1.0
+*/
+
 class OOPS{
 	public static void main(String args[]){
-		System.out.println("This is Dev Branch");
+		System.out.println("This is Use Case 1");
+		System.out.println("OOPs statement");
 	}
 }
