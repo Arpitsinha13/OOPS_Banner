8711b43797e9f0f822f99a816c772f104f9731f0
Arpitsinha13 <arpitbinaysinha07@gmail.com>
2026-03-10 13:24:51 +0530

    Update OOPS.java

--- OOPS.java
+++ OOPS.java
@@ -1,16 +1,14 @@
+//author Nitheesh
+//version 2
+    public class OOPSBannerUC2{
+    public static void main(String[] args) {
 
-class OOPS{
-	public static void main(String args[]){
-	   	   System.out.println(" **   **   **   ** ");
+        System.out.println(" *****   *****   *****   ***** ");
         System.out.println("*     * *     * *     * *      ");
         System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * * **    * *  ");
+        System.out.println("*     * *     * * ****    * *  ");
         System.out.println("*     * *     * *            * ");
         System.out.println("*     * *     * *            * ");
-        System.out.println(" **   **  *       ***  ");
+        System.out.println(" *****   *****  *       *****  ");
     }
-=======
-		System.out.println("This is Use Case 1");
-		System.out.println("OOPs statement");
-	}
-}v
+}
